58d220b0552c3f0eb2480c1dadd65244c6c9d50a
Heriniaina <heriniaina@passion4humanity.com>
2019-03-20 13:04:39 +0300

    Edit cv

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,8 @@ use App\Service\DefaultService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserType;
 
 class UserController extends AbstractController
 {
@@ -20,5 +22,27 @@ class UserController extends AbstractController
 			return $this->render('user/profile.html.twig', ['user' => $user]);
 		}
 		return $this->redirectToRoute('fos_user_security_login');
+    }
+
+    /**
+     * @Route("/profile/edit/{id}", name="user_profile_edit")
+     */
+    public function edit(Request $request, $id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    	$user = $em->getRepository('App:user')->find($id);
+        $form = $this->createForm(UserType::class, $user);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                $em->flush();
+            }
+        }
+    	return $this->render('user/edit_profile.html.twig', [
+    		'user' => $user,
+    		'form' => $form->createView()
+    		]);
     }
 }
---
+++ src/Service/UserService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    
+}
