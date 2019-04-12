8caf9b4f29c3813f0e373278651d7c32e82b6e62
Philippe Razakatsara <philippe@passion4humanity.com>
2019-04-12 08:51:57 +0300

    some fix

--- src/Form/TrainingType.php
+++ src/Form/TrainingType.php
@@ -123,6 +123,7 @@ class TrainingType extends AbstractType
                         'class' => 'form-control'
                     ]
                 ]
+                ]
             )
             ->add(
                 'school',
