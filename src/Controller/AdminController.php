cdb4fe09e0e54eebbb6925eb1a001f9e5f08500c
Philippe Razakatsara <philippe@passion4humanity.com>
2019-04-15 15:42:24 +0300

    Admin Ajout base du Dashboard Admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -144,4 +144,12 @@ class AdminController extends BaseAdminController
             ]
         );
     }
+
+    /**
+     * @Route("/dashboard", name="admin_dashboard")
+     */
+    public function dashboard()
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
 }
