33b59ecf90d096262de48702ba6775ba3ca67c23
Heriniaina <heriniaina@passion4humanity.com>
2019-04-18 13:27:49 +0300

    fix phpmd

--- src/Form/TrainingLevelType.php
+++ src/Form/TrainingLevelType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @SuppressWarnings(PHPMD)
+ */
 class TrainingLevelType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
